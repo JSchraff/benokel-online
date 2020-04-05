e13274de0ab8ccdde807a255f8f7d0f453f954df
Jan Schraff <js-171413@hs-weingarten.de>
2020-04-05 16:43:59 +0200

    functions test

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,16 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 import * as uuid from 'uuid';
 
+let app = admin.initializeApp();
 export const initialize_game = functions.https.onRequest((request, response) => {
+    let db = app.firestore();
+
 
 
     let gameData = {
         "id": uuid.v4(),
     };
+    db.collection("benokel-online").add(gameData);
     response.send(gameData);
 });
